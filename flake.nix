70529385b2f2ffd33023aa9fa2ea46c084be5a7c
Bulat <mbmhd2015@gmail.com>
2025-10-06 14:26:57 +0500

    Revert "Changed nixpkgs.url to unstable in flake.nix"
    This reverts commit 0c602c97b7fb66048c9aac730469c78dc5c5ba29.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS config with Home Manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; 
     hyprland.url = "github:hyprwm/Hyprland";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";

0c602c97b7fb66048c9aac730469c78dc5c5ba29
Bulat <mbmhd2015@gmail.com>
2025-10-06 12:16:17 +0500

    Changed nixpkgs.url to unstable in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS config with Home Manager";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; 
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
     hyprland.url = "github:hyprwm/Hyprland";
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";

1f6d70f2cf925ed29e0fc388db654b49118caa3e
Bulat Maskurov <mbmhd2015@gmail.com>
2026-05-04 16:24:18 +0300

    Small installation config

---
+++ bootstrap/configuration.nix
@@ -0,0 +1,63 @@
+{ config, lib, pkgs, ... }:
+
+let
+  disko = fetchTarball {
+    url = "https://github.com/nix-community/disko/archive/master.tar.gz";
+  };
+in
+{
+  imports = [
+    ./hardware-configuration.nix
+    "${disko}/module.nix"
+    ./disko.nix
+  ];
+
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 5;
+    xbootldrMountPoint = "/boot";
+  };
+  boot.loader.efi = {
+    canTouchEfiVariables = true;
+    efiSysMountPoint = "/efi";
+  };
+
+  networking.hostName = "thinkpad";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Moscow";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services.openssh = {
+    enable = true;
+    settings.PasswordAuthentication = true;
+    settings.PermitRootLogin = "no";
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  users.mutableUsers = true;
+  users.users.bulat = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    initialPassword = "nixos";
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    curl
+    wget
+    htop
+    tmux
+  ];
+
+  zramSwap = {
+    enable = true;
+    memoryPercent = 50;
+    algorithm = "zstd";
+  };
+
+  system.stateVersion = "25.11";
+}

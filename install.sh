7fbee2f947ac2ac26e4c51ada768ccf52bb6a8a1
Bulat Maskurov <mbmhd2015@gmail.com>
2026-05-04 11:38:22 +0300

    Fix nixos install tests

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo nix --experimental-features "nix-command flakes" run \
 sudo nixos-generate-config --no-filesystems --root /mnt \
   --show-hardware-config >hosts/thinkpad/hardware-configuration.nix
 
-sudo nixos-install --flake .#thinkpad
+sudo nixos-install --flake .#thinkpad \
+  --option extra-substituters "https://niri.cachix.org" \
+  --option extra-trusted-public-keys "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
 
 sudo nixos-enter --root /mnt -c 'passwd bulat'

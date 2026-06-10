43726591ff8690ec1981dc47c5f31639e8a08239
Bulat Maskurov <mbmhd2015@gmail.com>
2026-06-10 20:21:53 +0300

    Added amnezia alias

--- modules/home/bash/default.nix
+++ modules/home/bash/default.nix
@@ -13,6 +13,9 @@
       ld = "lazydocker";
 
       switch = "~/.nixos/rebuild.sh";
+
+      awgup = "sudo awg-quick up Netherlands";
+      awgdown = "sudo awg-quick down Netherlands";
     };
 
     sessionVariables = {
